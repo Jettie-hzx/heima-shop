36fcc6e4927fcb4c60eb88179001e222a812df15
月曜撰星辰 <2945037213@qq.com>
2025-02-19 14:01:12 +0800

    feat:完成首页热门推荐功能

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,8 +2,9 @@
 import { ref } from 'vue'
 import CustomNavbar from './components/CustomNavbar.vue'
 import CategoryPanel from './components/CategoryPanel.vue'
-import type { BannerItem, CategoryItem } from '@/types/home'
-import { getHomeBannerAPI, getHomeCategoryAPI } from '@/services/home'
+import HotPanel from './components/HotPanel.vue'
+import type { BannerItem, CategoryItem, HotItem } from '@/types/home'
+import { getHomeBannerAPI, getHomeCategoryAPI, getHomeHotAPI } from '@/services/home'
 import { onLoad } from '@dcloudio/uni-app'
 
 const bannerList = ref<BannerItem[]>([])
@@ -17,9 +18,16 @@ const getHomeCategory = async () => {
   const { result } = await getHomeCategoryAPI()
   categoryList.value = result
 }
+
+const hotList = ref<HotItem[]>([])
+const getHotList = async () => {
+  const { result } = await getHomeHotAPI()
+  hotList.value = result
+}
 onLoad(() => {
   getBannerList()
   getHomeCategory()
+  getHotList()
 })
 </script>
 
@@ -27,6 +35,7 @@ onLoad(() => {
   <CustomNavbar />
   <XtxSwiper :list="bannerList" />
   <CategoryPanel :list="categoryList" />
+  <HotPanel :list="hotList" />
   <view class="index"> </view>
 </template>
 
